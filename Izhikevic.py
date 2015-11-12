949998ede77f4c8d195172e7ae5ed95495feada0
Cherie Pun <cherie.cy.pun@gmail.com>
2015-11-12 22:54:07 +0000

    Changed the calculations of the parameters for inhibitory neurons, it is now the same as in the paper (google izchikevich simple model of spiking neurons)

--- Izhikevic.py
+++ Izhikevic.py
@@ -77,7 +77,7 @@ def ConnectIzhikevichNetworkLayers(CIJ, NExcitoryLayer, NInhibitoryLayer):
   network = IzNetwork([NExcitoryLayer, NInhibitoryLayer], Dmax)
 
   # Set neuron parameters for excitory layer
-  rand = rn.rand(NExcitoryLayer) * 0
+  rand = rn.rand(NExcitoryLayer)
   network.layer[0].N = NExcitoryLayer
   network.layer[0].a = 0.02 * np.ones(NExcitoryLayer)
   network.layer[0].b = 0.20 * np.ones(NExcitoryLayer)
@@ -110,12 +110,12 @@ def ConnectIzhikevichNetworkLayers(CIJ, NExcitoryLayer, NInhibitoryLayer):
       
       
   # Set neuron parameters for inhibitory layer
-  rand = rn.rand(NInhibitoryLayer) * 0
+  rand = rn.rand(NInhibitoryLayer)*0
   network.layer[1].N = NInhibitoryLayer
-  network.layer[1].a = 0.02 * np.ones(NInhibitoryLayer)
-  network.layer[1].b = 0.25 * np.ones(NInhibitoryLayer)
-  network.layer[1].c = -65 + 15*(rand**2)
-  network.layer[1].d = 2 - 6*(rand**2)
+  network.layer[1].a = 0.02 + 0.08*rand
+  network.layer[1].b = 0.25 - 0.05*rand
+  network.layer[1].c = -65 * np.ones(NInhibitoryLayer)
+  network.layer[1].d = 2 * np.ones(NInhibitoryLayer)
   
   ## Factor and delay
   network.layer[1].factor[0] = 50
